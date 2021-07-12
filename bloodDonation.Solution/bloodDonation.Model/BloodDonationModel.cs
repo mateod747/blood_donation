cf17096fdecf7d55c59d458f37f18dc9a1889fc5
Mateo Dubinjak <mateod747@gmail.com>
2021-07-12 21:51:05 +0200

    Models added

---
+++ bloodDonation.Solution/bloodDonation.Model/BloodDonationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloodDonation.Model
+{
+    public class BloodDonationModel
+    {
+        public int BloodID { get; set; }
+        public int RecipientID { get; set; }
+        public DateTime DateDonated { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ bloodDonation.Solution/bloodDonation.Model/BloodTransactionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloodDonation.Model
+{
+    public class BloodTransactionModel
+    {
+        public int TransactID { get; set; }
+        public int BloodID { get; set; }
+        public int RecipientID { get; set; }
+        public int EmpID { get; set; }
+        public DateTime DateOut { get; set; }
+        public int Quantity { get; set; }
+    }
+}
