c58880a684b39379e8a7bed3e4b47c5be5b291ce
Mateo Dubinjak <mateo.dubinjak@gmail.com>
2021-08-16 22:07:22 +0200

    login page css done

--- react-app/src/components/Login.js
+++ react-app/src/components/Login.js
@@ -3,19 +3,18 @@ import '../layouts/Login.css';
 import PropTypes from 'prop-types';
 
 async function LoginUser(credentials) {
-    return fetch('https://localhost:44336/api/login?username=' + credentials.username + '&password=' + credentials.password, 
-    {
-        method: 'GET',
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    })
-        .then(response => response.json())
-        .then(donorData =>
-            {
-                sessionStorage.setItem('loginToken', donorData.Token !== 'null' ? donorData.Token : '');
-                sessionStorage.setItem('donorID', donorData.DonorID);
+    return fetch('https://localhost:44336/api/login?username=' + credentials.username + '&password=' + credentials.password,
+        {
+            method: 'GET',
+            headers: {
+                "Access-Control-Allow-Origin": "*"
             }
+        })
+        .then(response => response.json())
+        .then(donorData => {
+            sessionStorage.setItem('loginToken', donorData.Token !== 'null' ? donorData.Token : '');
+            sessionStorage.setItem('donorID', donorData.DonorID);
+        }
         )
 }
 
@@ -29,28 +28,48 @@ export default function Login() {
             username,
             password
         })
-        if(sessionStorage.getItem('loginToken').length > 0) {
-            window.location.href='http://localhost:3000/dashboard';
+        if (sessionStorage.getItem('loginToken').length > 0) {
+            window.location.href = 'http://localhost:3000/dashboard';
         }
     }
     return (
-        <div className="login-wrapper">
-            <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" onChange={e => setUserName(e.target.value)} />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)} />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
+        <div className="box">
+            <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
+            <div class="container" id="container">
+                <div class="form-container sign-in-container">
+                    <form onSubmit={handleSubmit}>
+                        <h1>Sign in</h1>
+                        <input onChange={e => setUserName(e.target.value)} type="username" placeholder="Username" />
+                        <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" />
+                        <button type="submit">Sign In</button>
+                    </form>
                 </div>
-            </form>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button class="ghost" id="signIn">Sign In</button>
+                        </div>
+                        <div class="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button class="ghost" id="signUp">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <p>
+                    Created with <i class="fa fa-heart"></i> by
+                    <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
+                    - Read how I created this and how you can join the challenge
+                    <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
+                </p>
+            </footer>
         </div>
-    )
+    );
 }
 
 Login.propTypes = {
