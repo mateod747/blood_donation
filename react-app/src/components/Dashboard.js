dcf705dad7105310916505a7cac8743131932a76
Mateo Dubinjak <mateo.dubinjak@gmail.com>
2021-08-19 18:06:19 +0200

    dashboard at least looks like something

--- react-app/src/components/Dashboard.js
+++ react-app/src/components/Dashboard.js
@@ -1,7 +1,13 @@
-  
+
 import React, { Component } from 'react';
 import '../App.css';
 import '../layouts/Main.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 class Dashboard extends Component {
     constructor(props) {
@@ -11,32 +17,58 @@ class Dashboard extends Component {
         };
     }
 
-    fetchDonor = () => {   
-        let donorID = sessionStorage.getItem('donorID');      
-        fetch(`https://localhost:44336/api/donor/${donorID}`, 
-        {
-            method: 'GET',
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "token": sessionStorage.getItem('loginToken')
-            }
-        }) 
+    fetchDonor = () => {
+        let donorID = sessionStorage.getItem('donorID');
+        fetch(`https://localhost:44336/api/donor/${donorID}`,
+            {
+                method: 'GET',
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "token": sessionStorage.getItem('loginToken')
+                }
+            })
             .then(res => res.json())
             .then(json => {
                 this.setState({
                     item: json
-                });               
-            }); 
-        }       
-        
+                });
+            });
+    }
+
     componentDidMount() {
         this.fetchDonor();
-      }
+    }
 
     render() {
         return (
             <div className="dashboard">
-                <p>Name of Donor: {this.state.item.firstName} {this.state.item.lastName}</p>
+                <div className="dashboard-body ">
+                </div>
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+                    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                    crossorigin="anonymous"
+                />
+                <div className="name">
+                    {this.state.item.firstName} {this.state.item.lastName}
+                </div>
+                <div className="header">
+                    Osnovni podaci
+                </div>
+                <div className="header">
+                    <div className="line"></div>
+                </div>
+                <div className="dash">
+                    <div className="card">
+                        <div className="section">
+
+                        </div>
+                        <div className="section">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- react-app/src/components/Nav.js
+++ react-app/src/components/Nav.js
@@ -3,33 +3,30 @@ import '../App.css';
 import '../layouts/Nav.css'
 import { Link } from 'react-router-dom';
 import logo from '../public/logo-150.png';
+import styled from 'styled-components';
 
 function Nav() {
-
-    const navStyle = {
-        'font-family': 'monospace',
-        'color': 'rgb(132, 163, 163)',
-        'font-size': '20px',
-        'text-decoration': 'none'
-    };
-
     return (
         <nav>
             <div className="nav-links">
                 <div className="nav-logo">
-                    <img src={logo} />
+                    Sustav  <br></br>
+                    za <br></br>
+                    praćenje  <br></br>
+                    doniranja <br></br>
+                    krvi 
                 </div>
                 <div className="link-div">
-                    <div className="link"> 
-                    <Link style={navStyle} to="/dashboard">
-                        Dashboard
-                    </Link>
-                    </div>
-                    <div className="link">
-                    <Link style={navStyle} to="/dashboard">
-                        Dashboard
-                    </Link>
-                    </div>
+                    <a href="localhost:3000/dashboard">
+                        <div className="link">
+                            Osnovni podaci
+                        </div>
+                    </a>
+                    <a href="localhost:3000/dashboardx">
+                        <div className="link">
+                            Povijest doniranja
+                        </div>
+                    </a>
                 </div>
             </div>
         </nav>
